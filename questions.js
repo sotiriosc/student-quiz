b6fd3da1e2bedcaaba6a8ceaa1106703c36808b3
sotiri chortogiannos <chortogiannoss@gmail.com>
2023-01-17 14:40:33 -0500

    semi completed quiz

---
+++ questions.js
@@ -0,0 +1,83 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+      title: "The Console can be used to ",
+      choices: ["Debug", "fetch", "change", "do-while loops"],
+      answer: "Debug"
+    },
+    {
+      title: "The == and === are the same in Javascript",
+      choices: [
+        "True",
+        "False",
+       
+      ],
+      answer: "False"
+    },
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["booleans", "alerts", "numbers","strings"],
+      answer: "alerts"
+    },
+    {
+      title: "Boolean operators that can be used in JavaScript include:",
+      choices: [
+        "'And' Operator &&",
+        "'Or' Operator ||",
+        "'Not' Operator !",
+        "All the above"
+      ],
+      answer: "All the above"
+    },
+    {
+      title:
+        "Which one of these is not among the three different types of errors in JavaScript?",
+      choices: [
+        "Animation time errors",
+        "Load time errors",
+        "Run time errors",
+        "Logical Errors"
+      ],
+      answer: "Animation time errors"
+    },
+    {
+      title: "What is the data type of variables in JavaScript?",
+      choices: [
+        "Object data types",
+        "Function data type",
+        "None of the above",
+        "All of the above"
+      ],
+      answer: "Object data types"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store numbers, strings, arrays and boolean values.",
+      choices: [
+        "True",
+        "False",
+      ],
+      answer: "True"
+    },
+    {
+      title:
+        "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      answer: "quotes"
+    },
+    {
+      title:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+    },
+    {
+      title: "What is the type of Pop up boxes available in JavaScript?:",
+      choices: ["Alert", "Confirm", "Prompt", "All the above"],
+      answer: "All the above"
+    }
+  ];
