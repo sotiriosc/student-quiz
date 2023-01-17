03331de7dd86ac4deaaa7aee991a4fd0f79b397e
sotiri chortogiannos <chortogiannoss@gmail.com>
2023-01-17 15:19:52 -0500

    adjusted timer

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var feedbackEl = document.querySelector("#feedback");
 
 // quiz state variables
 var currentQuestionIndex = 0;
-var time = questions.length * 15;
+var time = questions.length * 20;
 var timerId;
 
 function startQuiz() {
